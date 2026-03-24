de9583b533e9b03dd7db623081561d483502dfc0
xsyvdsn64m-spec <xsyvdsn64m@privaterelay.appleid.com>
2026-03-24 17:10:22 -0400

    added LED code to loop
    defined out and in pins. created the randoms numbers. made the if statement for the LEDs in the rand1 smallest case

--- idolCode.ino
+++ idolCode.ino
@@ -1,44 +1,64 @@
-  // int rand1;
-  // int rand2;
-  // int rand3;
-  // int waitTime=5000;
-  #define B1 34 //will read in pin 13
-  #define B2 35
-  #define LED_PIN 2 //LED
+  int rand1;
+  int rand2;
+  int rand3;
+  int waitTime=5000;
+
+  //defining sensor pins
+  #define Strong1 36 
+  #define Weak1 39
+  #define Strong2 34 
+  #define Weak2 35
+  #define Strong3 32 
+  #define Weak3 33
+
+  //defining LED1 pins 
+  #define RED1 25
+  #define GREEN1 26 
+  #define BLUE1 27
+
+  //defining LED2 pins 
+  #define RED2 23
+  #define GREEN2 22
+  #define BLUE2 21
+
+  //defining LED2 pins 
+  #define RED3 19
+  #define GREEN3 18
+  #define BLUE3 17
+
+  #define LED_PIN 2 //LED on when correct sequence
 void setup() {
-  // //making random numbers
-  // rand1 = random(1,50); //generate a random number between 1 and 50
-  // rand2 = random(1,50); //generate a random number between 1 and 50
-  // rand3 = random(1,50); //generate a random number between 1 and 50
+  //making random numbers
+  randomSeed(analogRead(0)); //need to make numbers random every boot
+  rand1 = random(1,51); //generate a random number between 1 and 50
+  rand2 = random(1,51); //generate a random number between 1 and 50
+  rand3 = random(1,51); //generate a random number between 1 and 50
 
   pinMode(LED_PIN, OUTPUT); //setting the led to output
-  pinMode(B1, INPUT);
-  pinMode(B2, INPUT);
-  //to do
-  //find voltage values for LEDs corresponding to red, blue
-  //figure out how LEDs work
+  pinMode(Strong1, INPUT);
+  pinMode(Weak1, INPUT);
+
   
 
 }
 
 void loop() {
-  
-  int stateA = digitalRead(B1);
-  int stateB = digitalRead(B2);
-  if(stateA == HIGH && stateB == HIGH){
-    digitalWrite(LED_PIN, HIGH);
-  }else{
-    digitalWrite(LED_PIN,LOW);
-  }
-
-
-  // //rand1 the smallest var
-  // if(rand1<=rand2 && rand1<=rand3){
-  //     //set LED1 RED
-  //     //set LED2 BLUE
-  //     //set LED3 OFF
+  //rand1 the smallest var
+  if(rand1<=rand2 && rand1<=rand3){
+      //set LED1 RED
+        digitalWrite(RED1, HIGH);
+        digitalWrite(GREEN1, LOW);
+        digitalWrite(BLUE1, LOW);
+      //set LED2 BLUE
+        digitalWrite(RED2, LOW);
+        digitalWrite(GREEN2, LOW);
+        digitalWrite(BLUE2, HIGH);
+      //set LED3 OFF
+        digitalWrite(RED2, LOW);
+        digitalWrite(GREEN2, LOW);
+        digitalWrite(BLUE2, LOW);
     
-  //   delay(waitTime); //delay 5 seconds //will need to replace with a variable
+    delay(waitTime); //delay 5 seconds 
 
 
 
