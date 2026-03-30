95986fd974d2e7cc2398f56ac9732ee8af76a832
Braden Bosilovich <beb194@pitt.edu>
2026-03-29 21:09:16 -0400

    Some QOL additions to make including header files more simple

---
+++ Header Files/HEADERS_all.h
@@ -0,0 +1,8 @@
+/**
+ * Makes #include directive for all .h files in current working directory
+ */
+#include "error.h"
+#include "game_functions.h"
+#include "keypad.h"
+#include "OLED.h"
+#include "util.h"
