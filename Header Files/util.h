cb69913bb723837a04d3a827e482c37a5ab3cbc0
Braden Bosilovich <beb194@pitt.edu>
2026-03-30 16:25:40 -0400

    Functions for reading and writing registers to I2C device

--- Header Files/util.h
+++ Header Files/util.h
@@ -1,6 +1,7 @@
-#define INIT_COMMS 		0x55  // 0101 0101
-#define MASTER_ID  		0x65  // 0110 0101
-#define PERIPH1_ID 		0xA5  // 1010 0101
+#define UART_INIT_COMMS 		0x55  // 0101 0101
+#define UART_MASTER_ID  		0x65  // 0110 0101
+#define UART_PERIPH1_ID 		0xA5  // 1010 0101
+#define 
 
 void clearSerialBuffer()
 {
@@ -16,3 +17,21 @@ void decodeSerialLine()
 // Byte 2 = 01010101
 // Byte 3 = Peripheral ID
 // Byte 4 = Command
+
+
+void writeRegisterTo(uint8_t reg, uint8_t val, int slave_address)
+{
+  Wire.beginTransmission(slave_address);
+  Wire.write(reg);
+  Wire.write(val);
+  Wire.endTransmission();
+}
+
+uint8_t readRegisterFrom(uint8_t reg, int slave_address)
+{
+  Wire.beginTransmission(slave_address);
+  Wire.write(reg);
+  Wire.endTransmission(false);
+  Wire.requestFrom(slave_address, 1);
+  return Wire.read();
+}
