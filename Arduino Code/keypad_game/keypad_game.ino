ad300b144ae632c5cfc87fbd18cb19b85414a7f5
Braden Bosilovich <beb194@pitt.edu>
2026-04-02 17:05:52 -0400

    Fix all header and cpp files to work properly!!!! Keypad game works using all header files and namespace definitions.

--- Arduino Code/keypad_game/keypad_game.ino
+++ Arduino Code/keypad_game/keypad_game.ino
@@ -1,12 +1,20 @@
 #include <Wire.h>
 
-#define INCLUDE_OLED_1
+// #define INCLUDE_OLED_1
 // #define INCLUDE_OLED_2
 
-#include "MAIN_HEADER.h"
+#include "error.h"
+#include "game_functions.h"
+#include "keypad.h"
+#include "OLED_1.h"
+#include "timer.h"
+#include "util.h"
 
 // #define SCREEN_ADDRESS 0x3C
-#define RTC_INT_PIN        2
+#define RTC_INT_PIN       2
+#define OUT_PIN           3
+
+int numberKeypadSymbols = 4;
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,20 +23,20 @@ void setup() {
 
   OLED1::begin();
 
-  // pinMode(2, OUTPUT);
+  pinMode(OUT_PIN, OUTPUT);
   randomSeed(analogRead(A3));
   Timer::begin(RTC_INT_PIN);
 
-  Timer::startTimer(3000);
+  Timer::startTimer(5000);
   if (!Timer::isStarted())
     Error::raiseError("main", "setup", "timer not started successfully");
 }
 
 void loop() {
 
-  bool passed = Games::keypadChallenge(5);
+  bool passed = Games::keypadChallenge(numberKeypadSymbols);
   Timer::stopTimer();
-  digitalWrite(2, !passed);
+  digitalWrite(3, !passed);
   while(1);
 }
 
